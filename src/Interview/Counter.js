0acb4c2d206da81741c8d72d064150f6e46048c8
mmejia1 <45702570+mmejia1@users.noreply.github.com>
2021-05-23 17:32:08 -0400

    Update Counter.js

--- src/Interview/Counter.js
+++ src/Interview/Counter.js
@@ -5,7 +5,6 @@ function Counter(props){
   const array = props.users.filter((user) =>{
     //go into the selected key/value pair
     if (user.selected === true){
-      console.log('true',true)
       return user
     }
 
